c51da4714224db13708d655f8c2befb2edeecce0
ivan <ivanvalero1985@gmail.com>
2022-03-08 15:58:43 -0300

    Subiendo Archivos

---
+++ EcommerceApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  EcommerceApp
+//
+//  Created by Ivan Valero on 03/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            HomeScreen()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
